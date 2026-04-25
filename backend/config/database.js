ee6652ca778609d451f1d2a8023278e05f6747e8
pa360800-cmyk <pa360800@example.com>
2026-04-25 11:26:02 +0800

    fixingnetwork

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,15 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const databaseUrl = process.env.MYSQL_URL;
-const pool = mysql.createPool(databaseUrl);
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 module.exports = pool;
